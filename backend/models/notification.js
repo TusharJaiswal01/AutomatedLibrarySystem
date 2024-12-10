40084736ab8c36ca7e054e53cec186e3f76cde27
Tushar Jaiswal <tusharjaiswaltj01@gmail.com>
2024-12-10 17:53:18 +0530

    notification routes

--- backend/models/notification.js
+++ backend/models/notification.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+const NotificationSchema = new mongoose.Schema(
+
+    {
+        body:{
+            type: String,
+            
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export default mongoose.model("Notification",NotificationSchema);
